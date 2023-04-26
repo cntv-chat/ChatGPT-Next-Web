b17223eceb1628a5f5f3da0fd42454ee6b9e9cf5
Li Lin <129040015+cntv-chat@users.noreply.github.com>
2023-04-26 12:47:06 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "ChatGPT at cntv.chat",
+  description: "Your personal ChatGPT Chat Bot. cntv.chat welcome you!",
   appleWebApp: {
     title: "ChatGPT Next Web",
     statusBarStyle: "default",
